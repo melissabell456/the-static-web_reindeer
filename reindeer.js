5bd2ae8af37772a82fe4e7b9a7ab38fa296cf5ec
Melissa Bell <melissabell456@gmail.com>
2017-11-24 12:50:09 -0600

    finished exercise

---
+++ reindeer.js
@@ -0,0 +1,23 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+// Your task is to loop through all the reindeer in the array, and add the name of the reindeer to the single HTML <div> element provided. The name of the reindeer should be prepended with the corresponding color from the other array.
+
+// Example output:
+
+// Blue Dasher
+
+// Red Dancer
+function santa() {
+    var thisColor="";
+    var thisReindeer="";
+    var hohohoElement = document.getElementById("reindeer");
+    for (var i = 0; i < 8; i++) {
+       thisColor=colors[i];
+       thisReindeer=reindeer[i];
+        console.log(reindeer[i]);
+        console.log(colors[i]);
+        hohohoElement.innerHTML+="<div>" + thisColor + " " + thisReindeer + "</div>";
+    }
+};
+santa();
